b0e9ac1097ac0d7a24e6d971b41294217b5310a5
Jarif <xjarifx@gmail.com>
2024-10-25 23:23:09 +0600

    idk

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 
 interface NewsArticle {
   author: string | null;
@@ -26,99 +26,132 @@ export default function Home() {
   const [news, setNews] = useState<NewsArticle[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [page, setPage] = useState(1);
+  const observer = useRef<IntersectionObserver | null>(null);
 
-  useEffect(() => {
-    async function fetchData(url: string) {
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data: NewsResponse = await response.json();
-        console.log(data);
-        setNews(data.articles);
-      } catch (error: any) {
-        console.error(error);
-        setError(error.message);
-      } finally {
-        setLoading(false);
+  // Function to fetch news articles based on page
+  const fetchNews = useCallback(async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(`${url}&page=${page}&pageSize=10`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
-    }
-    fetchData(url);
-  }, [url]);
 
-  if (loading) {
-    return (
-      <div className="flex min-h-screen items-center justify-center">
-        <span className="loading loading-spinner loading-lg"></span>
-      </div>
-    );
-  }
+      const data: NewsResponse = await response.json();
+      console.log(data);
+      setNews((prevNews) => [
+        ...prevNews,
+        ...data.articles.filter(
+          (article) =>
+            article.author &&
+            article.source.name &&
+            article.title &&
+            article.description &&
+            article.urlToImage &&
+            article.url &&
+            article.publishedAt,
+        ),
+      ]); // Append new articles to existing list, filtering them
+    } catch (err) {
+      const errorMessage =
+        err instanceof Error ? err.message : "An error occurred";
+      console.error(errorMessage);
+      setError(errorMessage);
+    } finally {
+      setLoading(false);
+    }
+  }, [url, page]);
+
+  // Set up infinite scroll observer
+  const lastNewsElementRef = useCallback(
+    (node: HTMLDivElement | null) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading],
+  );
+
+  // Fetch initial news articles and subsequent pages when page changes
+  useEffect(() => {
+    fetchNews();
+  }, [fetchNews]);
+
   if (error) return <div>Error: {error}</div>;
 
   return (
     <main className="flex flex-col items-center justify-center px-6 text-neutral-950">
       <section>
-        {news.length > 0 ? (
-          news.map((singleNews, index) => (
-            <>
-              <article key={index} className="w-[800px]">
-                {/* author and source */}
-                <div className="mb-3 flex items-center justify-start">
-                  <img
-                    src="/panda.png" // Using <img> tag for local images
-                    alt="Author's logo"
-                    width={24} // Adjust width according to your needs
-                    height={24} // Adjust height according to your needs
-                    className="mr-2 inline-block"
-                  />
-                  <p className="inline-block text-xs">
-                    {singleNews.author || "Unknown Author"}{" "}
-                    <span className="inline text-neutral-400">from</span>{" "}
-                    {singleNews.source.name}
-                  </p>
-                </div>
-                {/* title and description */}
-                <div className="mb-3 flex">
-                  <div className="flex-1">
-                    <h2 className="mb-2 text-2xl font-bold leading-7">
-                      {singleNews.title}
-                    </h2>
-                    <p className="leading-5 text-neutral-500">
-                      {singleNews.description}
-                    </p>
-                  </div>
+        {news.map((singleNews, index) => (
+          <article
+            key={index}
+            className="w-[800px]"
+            ref={index === news.length - 1 ? lastNewsElementRef : null} // Attach ref to last item
+          >
+            {/* Author and source */}
+            <div className="mb-3 flex items-center justify-start">
+              <img
+                src="/panda.png"
+                alt="Author's logo"
+                width={24}
+                height={24}
+                className="mr-2 inline-block"
+              />
+              <p className="inline-block text-xs">
+                {singleNews.author}{" "}
+                <span className="inline text-neutral-400">from</span>{" "}
+                {singleNews.source.name}
+              </p>
+            </div>
 
-                  {singleNews.urlToImage && (
-                    <img
-                      src={singleNews.urlToImage} // Using <img> tag for external images
-                      alt={singleNews.title}
-                      className="h-36 w-36 ml-3 object-cover" // Control image size here
-                    />
-                  )}
-                </div>
-                {/* date and source page link */}
-                <div className="flex text-xs">
-                  <p className="mr-5">
-                    Published on:{" "}
-                    {new Date(singleNews.publishedAt).toLocaleDateString()}
-                  </p>{" "}
-                  <a
-                    href={singleNews.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-700 underline"
-                  >
-                    Read more
-                  </a>{" "}
-                </div>
-              </article>
-              {/* breaker */}
-              <hr className="my-8 h-[1px] w-full border-none bg-neutral-200" />
-            </>
-          ))
-        ) : (
-          <p>No news articles found.</p> // Message for no articles
+            {/* Title and description */}
+            <div className="mb-3 flex">
+              <div className="flex-1">
+                <h2 className="mb-2 text-2xl font-bold leading-7">
+                  {singleNews.title}
+                </h2>
+                <p className="leading-5 text-neutral-500">
+                  {singleNews.description}
+                </p>
+              </div>
+
+              <img
+                src={singleNews.urlToImage ?? "/placeholder-image.png"}
+                alt={singleNews.title}
+                className="ml-3 h-36 w-36 object-cover"
+              />
+            </div>
+
+            {/* Date and source page link */}
+            <div className="flex text-xs">
+              <p className="mr-5">
+                Published on:{" "}
+                {new Date(singleNews.publishedAt).toLocaleDateString()}
+              </p>
+              <a
+                href={singleNews.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-700 underline"
+              >
+                Read more
+              </a>
+            </div>
+
+            {/* Divider */}
+            <hr className="my-8 h-[1px] w-full border-none bg-neutral-200" />
+          </article>
+        ))}
+        {loading && (
+          <div className="flex items-center justify-center">
+            <span className="loading loading-spinner loading-lg"></span>
+          </div>
         )}
       </section>
     </main>
